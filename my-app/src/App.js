2b702ac6a3f62ad7d505cb7f5cef5a1985985be0
Chrislee3x7 <32285657+Chrislee3x7@users.noreply.github.com>
2023-01-21 11:50:30 -0500

    added academic side image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,18 @@
+import campusMap from './images/purdueCampus.png'
+
 import './App.css';
-import { GoogleMap, useLoadScript, Marker} from "@react-google-maps/api"
+// import { GoogleMap, useLoadScript, Marker} from "@react-google-maps/api"
 import { useMemo } from 'react';
 
 export default function App() {
   
-  const { isLoaded } = useLoadScript({googleMapsApiKey: "AIzaSyBpOAv7pgGXTnhaJ4PoG1hpyhdOFJ8a9II"});
+  // const { isLoaded } = useLoadScript({googleMapsApiKey: "AIzaSyBpOAv7pgGXTnhaJ4PoG1hpyhdOFJ8a9II"});
   
-  if(!isLoaded) return <div>Loading map...</div>
-  return <Map />;
-}
+  // if(!isLoaded) return <div>Loading map...</div>
+  return ( 
+    <div>
+      <img  src={campusMap} alt="purdue campus map"/>
+    </div>
 
-function Map(){
-
-  const center = useMemo(() => ({ lat: 44,  lng: 56}), []);
-
-  return <GoogleMap zoom={10} center={center} mapContainerClassName="map_container">
-    <Marker position={center} />
-  </GoogleMap>
+  );
 }
