293844c72784c6ccb1e9ae4cc522658a314c97f6
BraydenBrackett <bbracket@purdue.edu>
2023-01-21 16:34:55 -0500

    modified css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,29 +11,23 @@ import { Button, IconButton } from "@mui/material";
 export default function App() {
   const [isOpen, setIsOpen] = useState(false);
  
-  const toggleRatingPopup = () => {
-    setIsOpen(!isOpen);
-    //alert("woo");
-  }
+
   return (
   
     <div class="container">
       <img src={campusMap} alt="campus"/>
     
-        <Button class="toilet" id="arms" onClick={openRatingPage} style={{
-          minWidth: "25px",
-          minHeight: "25px"
-        }}>
+        <Button class="toilet" id="arms">
           <img
           src={Toilet} alt="toilet"
           />
         </Button>
 
-
-      <Button class="arms" onClick={toggleRatingPopup}>ARMS</Button>
-    
-      <RatingPopup></RatingPopup>
-      
+        <Button class="toilet" id="walc">
+          <img
+          src={Toilet} alt="toilet"
+          />
+        </Button>
     </div>
 
   );
