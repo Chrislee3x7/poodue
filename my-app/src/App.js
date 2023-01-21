a060b65793f9ff1a411761348131286aa478ecd1
BraydenBrackett <bbracket@purdue.edu>
2023-01-21 17:31:57 -0500

    All locations added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -35,6 +35,34 @@ export default function App() {
       <RatingPopup id="hamp"></RatingPopup>
       <RatingPopup id="walc"></RatingPopup>
       <RatingPopup id="arms"></RatingPopup>
+
+      <RatingPopup id="ston"></RatingPopup>
+      <RatingPopup id="univ"></RatingPopup>
+      <RatingPopup id="schm"></RatingPopup>
+      <RatingPopup id="mthw"></RatingPopup>
+      <RatingPopup id="brng"></RatingPopup>
+      <RatingPopup id="math"></RatingPopup>
+      <RatingPopup id="cl50"></RatingPopup>
+      <RatingPopup id="prce"></RatingPopup>
+      <RatingPopup id="psyc"></RatingPopup>
+      <RatingPopup id="sc"></RatingPopup>
+      <RatingPopup id="wthr"></RatingPopup>
+      <RatingPopup id="brwn"></RatingPopup>
+      <RatingPopup id="heav"></RatingPopup>
+      <RatingPopup id="stew"></RatingPopup>
+      <RatingPopup id="hiks"></RatingPopup>
+      <RatingPopup id="pmu"></RatingPopup>
+      <RatingPopup id="pmuc"></RatingPopup>
+      <RatingPopup id="gris"></RatingPopup>
+      <RatingPopup id="uc"></RatingPopup>
+      <RatingPopup id="baly"></RatingPopup>
+      <RatingPopup id="dudl"></RatingPopup>
+      <RatingPopup id="lmbs"></RatingPopup>
+      <RatingPopup id="knoy"></RatingPopup>
+      <RatingPopup id="potr"></RatingPopup>
+      <RatingPopup id="me"></RatingPopup>
+      <RatingPopup id="bhee"></RatingPopup>
+      <RatingPopup id="mrrt"></RatingPopup>
     </div>
 
   );
