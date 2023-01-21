74ec2e6c50d1f4efff3c74bc5b1c14d774ee7079
BraydenBrackett <bbracket@purdue.edu>
2023-01-21 16:21:16 -0500

    updated button tech

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,28 +7,16 @@ import styled from "styled-components";
 import { Button, IconButton } from "@mui/material";
 
 export default function App() {
-
-  //--------------------------------------
-
-  //padding is size
-  //border-raduis is how curvy it is
-  //margin is how you set the position
-
-  //--------------------------------------
-
+  
   return (
   
-    
     <div class="container">
       <img src={campusMap} alt="campus"/>
     
-        <Button class="toilet" id="arms" onClick={openRatingPage}>
-          <img
-          src={Toilet} alt="toilet"
-          />
-        </Button>
-
-        <Button class="toilet" id="walc" onClick={openRatingPage}>
+        <Button class="toilet" id="arms" onClick={openRatingPage} style={{
+          minWidth: "25px",
+          minHeight: "25px"
+        }}>
           <img
           src={Toilet} alt="toilet"
           />
